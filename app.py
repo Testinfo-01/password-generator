e64a576c52bff27fbee56b8e30c258eb99501fa0
Daniel Mukendi <danielmukendi@MacBookAir.uqo.local>
2025-04-08 21:24:16 -0400

    Ajout de l'historique des phrases + corrections de phrases complètes

--- app.py
+++ app.py
@@ -82,5 +82,6 @@ def index():
     return render_template("index.html", password=password)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=5001)
+
+
